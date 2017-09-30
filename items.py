4c61301f4c16481b87ab08b740ab8fc0797a7a00
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 23:05:00 +0200

    streamline needs and triggers

--- items.py
+++ items.py
@@ -5,24 +5,16 @@ pkg_dnf = {
 
 svc_systemd = {
     'nfs-server': {
-        'needs': [
-            'pkg_dnf:nfs-utils',
-        ],
+        'needs': ['pkg_dnf:nfs-utils'],
     },
     'rpcbind': {
-        'needs': [
-            'pkg_dnf:nfs-utils',
-        ],
+        'needs': ['pkg_dnf:nfs-utils'],
     },
     'rpc-statd': {
-        'needs': [
-            'pkg_dnf:nfs-utils',
-        ],
+        'needs': ['pkg_dnf:nfs-utils'],
     },
     'nfs-idmapd': {
-        'needs': [
-            'pkg_dnf:nfs-utils',
-        ],
+        'needs': ['pkg_dnf:nfs-utils'],
     },
 }
 
@@ -32,9 +24,7 @@ actions = {
     'nfs_export': {
         'command': 'exportfs -a',
         'triggered': True,
-        'needs': [
-            'pkg_dnf:nfs-utils',
-        ],
+        'needs': ['pkg_dnf:nfs-utils'],
     },
 }
 
@@ -46,14 +36,8 @@ for export in node.metadata['nfs-server']['exports']:
         'context': {
             'export': export,
         },
-        'needs': [
-            'pkg_dnf:nfs-utils',
-        ],
-        'triggers': [
-            'action:nfs_export',
-            'svc_systemd:nfs-server:restart',
-            'svc_systemd:rpcbind:restart',
-        ],
+        'needs': ['pkg_dnf:nfs-utils'],
+        'triggers': ['action:nfs_export', 'svc_systemd:nfs-server:restart', 'svc_systemd:rpcbind:restart'],
     }
 
 if node.has_bundle('firewalld'):
@@ -63,34 +47,22 @@ if node.has_bundle('firewalld'):
                 'command': 'firewall-cmd --permanent --zone={} --add-service=nfs'.format(zone),
                 'unless': 'firewall-cmd --zone={} --list-services | grep nfs'.format(zone),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
             actions['firewalld_add_mountd_zone_{}'.format(zone)] = {
                 'command': 'firewall-cmd --permanent --zone={} --add-service=mountd'.format(zone),
                 'unless': 'firewall-cmd --zone={} --list-services | grep mountd'.format(zone),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
             actions['firewalld_add_rpc-bind_zone_{}'.format(zone)] = {
                 'command': 'firewall-cmd --permanent --zone={} --add-service=rpc-bind'.format(zone),
                 'unless': 'firewall-cmd --zone={} --list-services | grep rpc-bind'.format(zone),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
     elif node.metadata.get('firewalld', {}).get('default_zone'):
         default_zone = node.metadata.get('firewalld', {}).get('default_zone')
@@ -98,34 +70,22 @@ if node.has_bundle('firewalld'):
             'command': 'firewall-cmd --permanent --zone={} --add-service=nfs'.format(default_zone),
             'unless': 'firewall-cmd --zone={} --list-services | grep nfs'.format(default_zone),
             'cascade_skip': False,
-            'needs': [
-                'pkg_dnf:firewalld',
-            ],
-            'triggers': [
-                'action:firewalld_reload',
-            ],
+            'needs': ['pkg_dnf:firewalld'],
+            'triggers': ['action:firewalld_reload'],
         }
         actions['firewalld_add_mountd_zone_{}'.format(default_zone)] = {
             'command': 'firewall-cmd --permanent --zone={} --add-service=mountd'.format(default_zone),
             'unless': 'firewall-cmd --zone={} --list-services | grep mountd'.format(default_zone),
             'cascade_skip': False,
-            'needs': [
-                'pkg_dnf:firewalld',
-            ],
-            'triggers': [
-                'action:firewalld_reload',
-            ],
+            'needs': ['pkg_dnf:firewalld'],
+            'triggers': ['action:firewalld_reload'],
         }
         actions['firewalld_add_rpc-bind_zone_{}'.format(default_zone)] = {
             'command': 'firewall-cmd --permanent --zone={} --add-service=rpc-bind'.format(default_zone),
             'unless': 'firewall-cmd --zone={} --list-services | grep rpc-bind'.format(default_zone),
             'cascade_skip': False,
-            'needs': [
-                'pkg_dnf:firewalld',
-            ],
-            'triggers': [
-                'action:firewalld_reload',
-            ],
+            'needs': ['pkg_dnf:firewalld'],
+            'triggers': ['action:firewalld_reload'],
         }
     elif node.metadata.get('firewalld', {}).get('custom_zones', False):
         for interface in node.metadata['interfaces']:
@@ -134,66 +94,42 @@ if node.has_bundle('firewalld'):
                 'command': 'firewall-cmd --permanent --zone={} --add-service=nfs'.format(custom_zone),
                 'unless': 'firewall-cmd --zone={} --list-services | grep nfs'.format(custom_zone),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
             actions['firewalld_add_mountd_zone_{}'.format(custom_zone)] = {
                 'command': 'firewall-cmd --permanent --zone={} --add-service=mountd'.format(custom_zone),
                 'unless': 'firewall-cmd --zone={} --list-services | grep mountd'.format(custom_zone),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
             actions['firewalld_add_rpc-bind_zone_{}'.format(custom_zone)] = {
                 'command': 'firewall-cmd --permanent --zone={} --add-service=rpc-bind'.format(custom_zone),
                 'unless': 'firewall-cmd --zone={} --list-services | grep rpc-bind'.format(custom_zone),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:firewalld',
-                ],
-                'triggers': [
-                    'action:firewalld_reload',
-                ],
+                'needs': ['pkg_dnf:firewalld'],
+                'triggers': ['action:firewalld_reload'],
             }
     else:
         actions['firewalld_add_nfs'] = {
             'command': 'firewall-cmd --permanent --add-service=nfs',
             'unless': 'firewall-cmd --list-services | grep nfs',
             'cascade_skip': False,
-            'needs': [
-                'pkg_dnf:firewalld',
-            ],
-            'triggers': [
-                'action:firewalld_reload',
-            ],
+            'needs': ['pkg_dnf:firewalld'],
+            'triggers': ['action:firewalld_reload'],
         }
         actions['firewalld_add_mountd'] = {
             'command': 'firewall-cmd --permanent --add-service=mountd',
             'unless': 'firewall-cmd --list-services | grep mountd',
             'cascade_skip': False,
-            'needs': [
-                'pkg_dnf:firewalld',
-            ],
-            'triggers': [
-                'action:firewalld_reload',
-            ],
+            'needs': ['pkg_dnf:firewalld'],
+            'triggers': ['action:firewalld_reload'],
         }
         actions['firewalld_add_rpc-bind'] = {
             'command': 'firewall-cmd --permanent --add-service=rpc-bind',
             'unless': 'firewall-cmd --list-services | grep rpc-bind',
             'cascade_skip': False,
-            'needs': [
-                'pkg_dnf:firewalld',
-            ],
-            'triggers': [
-                'action:firewalld_reload',
-            ],
+            'needs': ['pkg_dnf:firewalld'],
+            'triggers': ['action:firewalld_reload'],
         }
